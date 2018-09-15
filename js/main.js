ad6cbd0264e871d189d6003e5f4ff05cedd34a38
Tom McNichol <tlmcnichol@gmail.com>
2018-09-14 21:40:31 -0600

    Completed accessibility requirements

--- js/main.js
+++ js/main.js
@@ -169,6 +169,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'Restaurant Image';
   li.append(image);
 
   const name = document.createElement('h1');
